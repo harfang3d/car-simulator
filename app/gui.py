bcccc3f39a6617dfaffdc0074e33bb86e21eb7d6
Clément <beudotc@gmail.com>
2022-11-04 14:56:37 +0100

    Fix #3
    Controls with keyboard + better naming / car settings

--- app/gui.py
+++ app/gui.py
@@ -1,20 +1,21 @@
 import harfang as hg
 from utils import MetersPerSecondToKMH
 
-def DrawGui(res_x, res_y, dt, dts, car_vel, vid, debug_physic, car_pos, debug_car):
+def DrawGui(res_x, res_y, dt, dts, car_vel, vid, debug_physic, car_pos, debug_car, control_keyboard):
 	hg.ImGuiBeginFrame(res_x, res_y, dt, hg.ReadMouse(), hg.ReadKeyboard())
 	hg.ImGuiBegin("Debug", True, hg.ImGuiWindowFlags_NoMove | hg.ImGuiWindowFlags_NoResize)
-	hg.ImGuiSetWindowSize("Debug", hg.Vec2(350, 150), hg.ImGuiCond_Once)
+	hg.ImGuiSetWindowSize("Debug", hg.Vec2(350, 180), hg.ImGuiCond_Once)
 	hg.ImGuiText("dt = " + str(round(dts, 4)))
 	hg.ImGuiText("speed = " + str(round(MetersPerSecondToKMH(hg.Len(car_vel)))) + " km/h")
 	hg.ImGuiText("car position = Vec3(" + str(round(car_pos.x, 4)) + ", " + str(round(car_pos.y, 4)) + ", " + str(round(car_pos.z, 4)) + ")")
 	was_clicked, debug_physic = hg.ImGuiCheckbox('Physic Debug', debug_physic)
 	was_clicked, debug_car = hg.ImGuiCheckbox('Car Debug', debug_car)
+	was_clicked, control_keyboard = hg.ImGuiCheckbox('Keyboard Control', control_keyboard)
 
 	hg.ImGuiEnd()
 	hg.ImGuiEndFrame(vid)
 
-	return debug_physic, debug_car
+	return debug_physic, debug_car, control_keyboard
 
 def DrawLine(pos_a, pos_b, line_color, vid, vtx_line_layout, line_shader):
 	vtx = hg.Vertices(vtx_line_layout, 2)
