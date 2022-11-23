6d8a69cc9fbba94f46ce5adfd169e7f16b8b443a
Clément <91607630+disketflu@users.noreply.github.com>
2022-11-23 11:54:28 +0100

    commit function to extract data from path converter folder

--- app/car_spawner.py
+++ app/car_spawner.py
@@ -1,4 +1,5 @@
 import harfang as hg
+import json, os
 from utils import *
 
 def CreateNewCar(scene, res, pos):
@@ -12,3 +13,24 @@ def HandleCarMovement(carnode, physics):
 	car_pos.z += 0.65
 	carnode_transform.SetPos(car_pos)
 	# physics.NodeWake(carnode)
+ 
+def GetBlockTracks():
+	file_dir_out = "out/"
+	path = os.getcwd() + "/../tools/path_converter/" + file_dir_out
+ 
+	files = []
+	
+	for r, d, f in os.walk(path):
+		for file in f:
+				files.append(os.path.join(r, file))
+
+	final_data = {}
+ 
+	for f in files:
+		with open(f, 'r') as json_file:
+			file_name = os.path.basename(f).replace(".json", "")
+			node_name = file_name.replace("_tracks", "")
+			data = json.loads(json_file.read())
+			final_data[node_name] = data
+	
+	return final_data
